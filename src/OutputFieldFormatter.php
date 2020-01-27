2bfac18728bdc1faefd9ac6f96425fdfc602d7d8
Luke <luke_redfern@hotmail.com>
2020-01-27 15:20:24 +0000

    Update OutputFieldFormatter

---
+++ src/OutputFieldFormatter.php
@@ -0,0 +1,136 @@
+<?php
+/**
+ * OutputFieldFormatter class file
+ *
+ * Requires PHP version 7
+ *
+ * @category Class
+ * @package  Andyredfern\Fixedwidth
+ * @author   Andy Redfern <and@redfern.it>
+ * @license  MIT License (MIT)
+ * @link     https://github.com/andyredfern/fixedwidth
+ */
+namespace Andyredfern\Fixedwidth;
+
+/**
+ * OutputFieldFormatter Class for fomattimg a variable as fixed length padded string
+ *
+ * @category Class
+ * @package  Andyredfern\Fixedwidth
+ * @author   Andy Redfern <and@redfern.it>
+ * @license  MIT License (MIT)
+ * @link     https://github.com/andyredfern/fixedwidth
+ */
+class OutputFieldFormatter
+{
+
+    /**
+     * Public function that process the $value and returns a
+     * formatted, fixed-length string
+     *
+     * @param $value     can be a string, integer, floating point or date
+     * @param array  $fieldSpec The parameters to control the output
+     * @param string $padString string used to pad.
+     *
+     * @return string formatted string of the submitted $value
+     */
+    public static function format($value, $fieldSpec, $padString)
+    {
+        switch ($fieldSpec["type"]) {
+        case "i": // integer
+            $valueString = OutputFieldFormatter::_formatInteger($value);
+            break;
+        case "f": // floating point
+            $valueString = OutputFieldFormatter::_formatFloat(
+                $value,
+                $fieldSpec["format"]
+            );
+            break;
+        case "s": // string
+            $valueString = $value;
+            break;
+        case "d": // date
+            $valueString = OutputFieldFormatter::_formatDate(
+                $value,
+                $fieldSpec["format"]
+            );
+            break;
+        }
+        return OutputFieldFormatter::_normaliseString(
+            $valueString,
+            $fieldSpec["len"],
+            $padString,
+            $fieldSpec["align"]
+        );
+    }
+
+    /**
+     * Format integer as string.
+     *
+     * @param $value integer to be formatted.
+     *
+     * @return string formatted string.
+     */
+    private static function _formatInteger($value): string
+    {
+        if (empty($value)) {
+            return "";
+        }
+        return strval($value);
+    }
+
+    /**
+     * Format float as a string.
+     *
+     * @param $value  float to be formatted.
+     * @param string $format format string. See sprintf().
+     *
+     * @return string formatted string.
+     */
+    private static function _formatFloat(
+        $value,
+        string $format = "%0.2f"
+    ): string {
+        if (empty($value)) {
+            return "";
+        }
+        return sprintf($format, $value);
+    }
+
+    /**
+     * Format date as string.
+     *
+     * @param $value  date to be formatted
+     * @param string $format formatted string.
+     *
+     * @return string formatted string.
+     */
+    private static function _formatDate($value, string $format = "Ymd"): string
+    {
+        if (empty($value)) {
+            return "";
+        }
+        return date($format, strtotime($value));
+    }
+
+    /**
+     * Normalises string length by padding and trimming.
+     *
+     * @param string $value     string to be normalised.
+     * @param int    $length    target length of string.
+     * @param string $padString string to be used to pad.
+     * @param string $align     alignment for of the string within padding.
+     *
+     * @return string string of normalised length.
+     */
+    private static function _normaliseString(
+        string $value,
+        int $length,
+        string $padString,
+        string $align = "right"
+    ) {
+        $padType = $align == "left" ? STR_PAD_RIGHT : STR_PAD_LEFT;
+        $paddedValue = str_pad($value, $length, $padString, $padType);
+        return substr($paddedValue, 0, $length);
+    }
+}
---
+++ tests/OutputFieldFormatterTest.php
@@ -0,0 +1,194 @@
+<?php
+/**
+ * OutputFieldTest PHPUnit Tests for OutputField class
+ *
+ * Requires PHP version 7
+ *
+ * @category Tests
+ * @package  Andyredfern\Fixedwidth
+ * @author   Andy Redfern <and@redfern.it>
+ * @license  MIT License (MIT)
+ * @link     https://github.com/andyredfern/fixedwidth
+ */
+namespace Andyredfern\Fixedwidth;
+
+/**
+ * OutputFieldTest PHPUnit Tests for OutputField class
+ *
+ * @category Tests
+ * @package  Andyredfern\Fixedwidth
+ * @author   Andy Redfern <and@redfern.it>
+ * @license  MIT License (MIT)
+ * @link     https://github.com/andyredfern/fixedwidth
+ */
+class OutputFieldFormatterTest extends \PHPUnit\Framework\TestCase
+{
+
+    /**
+     * Test integers are returned as correctly formatted string
+     *
+     * @return unused
+     * @test
+     */
+    public function formatVariousIntegers()
+    {
+
+        $fieldType = array(
+            "len" => "10",
+            "type" => "i",
+            "align" => "right",
+            "name" => "field1");
+        $integer = 26;
+
+        $result = OutputFieldFormatter::format($integer, $fieldType, "0");
+
+        $this->assertEquals($result, "0000000026");
+
+        $fieldType = array(
+            "len" => "8",
+            "type" => "i",
+            "align" => "right",
+            "name" => "field1");
+        $integer = 3124;
+
+        $result = OutputFieldFormatter::format($integer, $fieldType, "0");
+
+        $this->assertEquals($result, "00003124");
+
+        $fieldType = array(
+            "len" => "8",
+            "type" => "i",
+            "align" => "right",
+            "name" => "field1");
+        $integer = 199;
+
+        $result = OutputFieldFormatter::format($integer, $fieldType, " ");
+
+        $this->assertEquals($result, "     199");
+
+    }
+
+    /**
+     * Test floats are returned as correctly formatted string
+     *
+     * @return unused
+     * @test
+     */
+    public function formatVariousFloats()
+    {
+        $fieldType = array(
+            "len" => "9",
+            "type" => "f",
+            "align" => "right",
+            "name" => "field1",
+            "format" => "%04.4f");
+        $float = 12.34;
+
+        $result = OutputFieldFormatter::format($float, $fieldType, "0");
+
+        $this->assertEquals($result, "0012.3400");
+
+        $fieldType = array(
+            "len" => "9",
+            "type" => "f",
+            "align" => "right",
+            "name" => "field1",
+            "format" => "%0.2f");
+        $float = 12.3456;
+
+        $result = OutputFieldFormatter::format($float, $fieldType, " ");
+
+        $this->assertEquals($result, "    12.35");
+    }
+
+    /**
+     * Test dates are returned as correctly formatted string
+     *
+     * @return unused
+     * @test
+     */
+    public function formatVariousDates()
+    {
+
+        $fieldType = array(
+            "len" => "8",
+            "type" => "d",
+            "align" => "right",
+            "name" => "field1",
+            "format" => "Ymd");
+        $date = "12 June 2021";
+
+        $result = OutputFieldFormatter::format($date, $fieldType, " ");
+
+        $this->assertEquals($result, "20210612");
+
+        $fieldType = array(
+            "len" => "8",
+            "type" => "d",
+            "align" => "right",
+            "name" => "field1",
+            "format" => "Ymd");
+        $date = "";
+
+        $result = OutputFieldFormatter::format($date, $fieldType, " ");
+
+        $this->assertEquals($result, str_pad(" ", 8));
+
+        $fieldType = array(
+            "len" => "10",
+            "type" => "d",
+            "align" => "right",
+            "name" => "field1",
+            "format" => "Y-m-d");
+        $date = "12 May 2022";
+
+        $result = OutputFieldFormatter::format($date, $fieldType, " ");
+
+        $this->assertEquals($result, "2022-05-12");
+
+    }
+
+    /**
+     * Test strings are returned as correctly formatted string
+     *
+     * @return unused
+     * @test
+     */
+    public function formatVariousStrings()
+    {
+
+        $fieldType = array(
+            "len" => "8",
+            "type" => "s",
+            "align" => "left",
+            "name" => "field1");
+        $str = "Hello";
+
+        $result = OutputFieldFormatter::format($str, $fieldType, " ");
+
+        $this->assertEquals($result, "Hello   ");
+
+        $fieldType = array(
+            "len" => "10",
+            "type" => "s",
+            "align" => "right",
+            "name" => "field1");
+        $str = "HelloHello";
+
+        $result = OutputFieldFormatter::format($str, $fieldType, " ");
+
+        $this->assertEquals($result, "HelloHello");
+
+        $fieldType = array(
+            "len" => "5",
+            "type" => "s",
+            "align" => "right",
+            "name" => "field1");
+        $str = "HelloHello";
+
+        $result = OutputFieldFormatter::format($str, $fieldType, " ");
+
+        $this->assertEquals($result, "Hello");
+
+    }
+}
